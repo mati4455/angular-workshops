c8ae7e439c18cec1508e0d9ae45507539df254c3
Mateusz Pacholec <mateuszpacholec@gmail.com>
2018-09-21 09:24:49 +0200

    Add input to enter new task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { CurrentDatetimeComponent } from './current-datetime/current-datetime.component';
 import { TodoListElementComponent } from './todo-list-element/todo-list-element.component';
+import { TodoInputComponent } from './todo-input/todo-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoListComponent,
     CurrentDatetimeComponent,
-    TodoListElementComponent
+    TodoListElementComponent,
+    TodoInputComponent
   ],
   imports: [
     BrowserModule
